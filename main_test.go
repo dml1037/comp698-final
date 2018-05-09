c56c01870652a967a9fa694468392d35deed94e0
Danielle Marie <danielle.m.leboeuf@gmail.com>
2018-05-09 18:51:40 -0400

    updating changes from all these files

--- main_test.go
+++ main_test.go
@@ -1,19 +1,21 @@
-package main
-
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
-)
-
-func TestHandler(t *testing.T) {
+ 	"net/http"
+ 	"net/http/httptest"
+	"strings"
+ 	"testing"
+ )
+ 
+ func TestHandler(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/", nil)
-	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/home", nil)
+ 	response := httptest.NewRecorder()
 	hello(response, request)
-
+	IndexHandler(response, request)
+ 
 	expected := "Hello, World!"
 	if response.Body.String() != expected {
-		t.Fatalf("Got: %s\nExpected: %s",
-			response.Body.String(), expected)
-	}
-}
+	expected := "COMP698 Final Project with Bootstrap"
+	if !strings.Contains(response.Body.String(), expected) {
+ 		t.Fatalf("Got: %s\nExpected: %s",
+ 			response.Body.String(), expected)
+ 	}
